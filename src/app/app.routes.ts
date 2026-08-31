92d016eecf4722ce4f4c38372a5d894b31b1ef49
guilhermeAqr <guilhermeaquinofr@gmail.com>
2026-08-31 16:51:44 -0300

    mudança no routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { HOST_TAG_NAME } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: "home", component:Home},
+    {path: "login", component:Login },
+    {path: "dashboard", component:Dashboard },
+    {path: redirectTo:"Login", pathMatch:"full"}
+    
+];
