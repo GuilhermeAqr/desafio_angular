44aa164bdfe3ebbfd026bdb5448a678f933dd8fa
GuilhermeAqr <guilhermeaquinofr@gmail.com>
2026-09-17 03:39:48 -0300

    adicao de Contato e melhoria nas paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { authGuard } from './guard/auth-guard';
 export const routes: Routes = [
 
     {path: "home", component:Home, canActivate:[authGuard]},
+    {path: "contato", component:Contato },
     {path: "login", component:Login },
     {path: "dashboard", component:Dashboard },
     {path: "", redirectTo:"Login", pathMatch:"full"}
